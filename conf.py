35b4ed82f1174e470df46d009e7f8fc91d83d131
LiYanrui <liyanrui.m2@gmail.com>
2018-04-28 08:47:56 +0800

    wip

--- conf.py
+++ conf.py
@@ -942,7 +942,7 @@ LICENSE = ""
 
 # A small copyright notice for the page footer (in HTML).
 # (translatable)
-CONTENT_FOOTER = 'Contents &copy; {date}         <a href="mailto:{email}">{author}</a> - Powered by         <a href="https://getnikola.com" rel="nofollow">Nikola</a>         {license}'
+CONTENT_FOOTER = '本站内容 &copy; {date} {author}({email})'
 
 # Things that will be passed to CONTENT_FOOTER.format().  This is done
 # for translatability, as dicts are not formattable.  Nikola will
