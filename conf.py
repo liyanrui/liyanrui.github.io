4c617f7affc661fe468282457a72b6d0df78b210
LiYanrui <liyanrui.m2@gmail.com>
2018-04-28 10:09:32 +0800

    Nikola auto commit.
    Nikola version: 7.8.14

--- conf.py
+++ conf.py
@@ -211,7 +211,7 @@ DATE_FORMAT = '%m-%d'
 
 # Date format used to display post dates, if local dates are used. (translatable)
 # (str used by moment.js)
-JS_DATE_FORMAT = 'MM-DD'
+JS_DATE_FORMAT = 'YYYY-MM-DD HH:mm'
 
 # Date fanciness.
 #
