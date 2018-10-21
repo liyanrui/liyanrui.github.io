ba6b3407ebd20bf96631dae2ed1b4687ab119fa2
LiYanrui <liyanrui.m2@gmail.com>
2018-10-21 10:40:39 +0800

    Nikola auto commit.
    Nikola version: 8.0.1

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Garfileo"  # (translatable)
 BLOG_TITLE = "NULL"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "/"
+SITE_URL = "/index.html"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
