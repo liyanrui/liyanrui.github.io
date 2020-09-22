2746074a09b91d25f4b7b6f8011e15ab4ddddc9b
liyanrui <liyanrui.m2@gmail.com>
2020-09-22 08:59:15 +0800

    Nikola auto commit.
    Nikola version: 8.1.1

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Garfileo"  # (translatable)
-BLOG_TITLE = "唱歌的人不许掉眼泪"  # (translatable)
+BLOG_TITLE = "# rm -rf /"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "/"
